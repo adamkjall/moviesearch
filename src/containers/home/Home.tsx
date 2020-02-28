4bf7af7f8926950903e2d023f3d4f7bbb6041046
Stenis76 <stenqvist.dick@gmail.com>
2020-02-28 11:24:10 +0100

    grundkomponenter uppsatta

---
+++ src/containers/home/Home.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import Header from "../../components/header/Header"
+import MainContent from "../../components/main_content/MainContent"
+import Sidebar from "../../components/sidebar/Sidebar"
+
+
+class Home extends React.Component {
+
+  render() {
+    return (
+      <div>
+
+      <Header />
+      <MainContent />
+      <Sidebar />
+        
+      </div>
+      
+    )
+  };
+} 
+
+
+
+
+
+
+
+export default Home 
+
