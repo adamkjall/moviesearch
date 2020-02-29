7ff5a84e4ca8618d7b8fcaec137a73e12a43bb74
Fredrik Thelin <fredrikthelin82@gmail.com>
2020-02-29 21:24:34 +0100

    styled sidebar and changed background

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./sidebarStyle.css"
+import Navbar from '../navbar/Navbar'
 
 class Sidebar extends React.Component {
 
   render() {
     return (
       <div className="sidebarContainer">
-        <p>Sidebar</p>
+        <Navbar />  
       </div>
     )
   };
