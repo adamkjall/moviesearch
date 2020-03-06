61ff764e052ab2243014f8a3f309d225963aac0b
Stenis76 <stenqvist.dick@gmail.com>
2020-03-06 10:26:58 +0100

    rensat gammal kod

--- src/components/searchbar/searchbar.tsx
+++ src/components/searchbar/searchbar.tsx
@@ -33,7 +33,6 @@ class SearchBar extends React.Component<Props, State> {
     this.setState({ value: event.target.value });
   }
 
-  //avfyra funktionen från app.tsx - callback
   handleKeyPress(event: any) {
     if (event.which === 13) {
       this.props.getMoviesFromSearch(this.state.value);
@@ -41,52 +40,24 @@ class SearchBar extends React.Component<Props, State> {
   }
 
   render() {
-    // const { active, value, /*error,*/ label } = this.state;
-    // const { predicted, locked } = this.props;
-    // const fieldClassName = `field ${(locked ? active : active || value) &&
-    //   "active"} ${locked && !active && "locked"}`;
-
     return (
-      // <div className={fieldClassName}>
-      //   {active && value && predicted && predicted.includes(value) && (
-      //     <p className="predicted">{predicted}</p>
-      //   )}
-      // <div className="field">
-      /* <input
-          // id={1}
-          type="text"
-          placeholder={label}
-          onChange={this.changeValue.bind(this)}
-          onKeyPress={this.handleKeyPress.bind(this)}
-          onFocus={() => !locked && this.setState({ active: true })}
-          onBlur={() => !locked && this.setState({ active: false })}
-        /> */
-      /* <label htmlFor={1} className={error && "error"}>
-      {error || label}
-      </label> */
       <div>
         {this.state.expanded ? (
           <div className="field field-active">
             <input
-              // id={1}
               type="text"
               placeholder={" Vad vill du se idag?"}
               onChange={this.changeValue.bind(this)}
               onKeyPress={this.handleKeyPress.bind(this)}
-              // onFocus={() => !locked && this.setState({ active: true })}
-              // onBlur={() => !locked && this.setState({ active: false })}
             />
           </div>
         ) : (
           <div className="field">
             <input
-              // id={1}
               type="text"
               placeholder={""}
               onChange={this.changeValue.bind(this)}
               onKeyPress={this.handleKeyPress.bind(this)}
-              // onFocus={() => !locked && this.setState({ active: true })}
-              // onBlur={() => !locked && this.setState({ active: false })}
             />
           </div>
         )}
