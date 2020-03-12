d16048dea8e4e6f607aac41d2d0612ceec7e7870
Adam <adam.kjall@hotmail.com>
2020-03-12 09:24:36 +0100

    fixed merge

--- src/utils/themoviedb-api.js
+++ src/utils/themoviedb-api.js
@@ -1,64 +1,44 @@
 const baseUrl = "https://api.themoviedb.org/3/";
 export const baseImgUrl = "https://image.tmdb.org/t/p/original";
 
-export const fetchTrendingMovies = async () => {
-  try {
-    const response = await fetch(
-      `${baseUrl}trending/movie/day?api_key=${process.env.REACT_APP_API}`
-    );
-    const data = await response.json();
-    return data.results;
-  } catch (error) {
-    console.log(error);
-  }
+export const fetchTrendingMovies = async (page = 1) => {
+  const response = await fetch(
+    `${baseUrl}trending/movie/day?api_key=${process.env.REACT_APP_API}&language=en-US&page=${page}`
+  );
+  const data = await response.json();
+  return data;
 };
 
-export const fetchPopularMovies = async () => {
-  try {
-    const response = await fetch(
-      `${baseUrl}discover/movie?api_key=${process.env.REACT_APP_API}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
-    );
-    const data = await response.json();
-    return data.results;
-  } catch (error) {
-    console.log(error);
-  }
+export const fetchPopularMovies = async (page = 1) => {
+  const response = await fetch(
+    `${baseUrl}discover/movie?api_key=${process.env.REACT_APP_API}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+  );
+  const data = await response.json();
+  return data;
 };
 
-export const fetchNewMovies = async () => {
-  try {
-    const response = await fetch(
-      `${baseUrl}discover/movie?api_key=${process.env.REACT_APP_API}&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&release_date.lte=2020-03-08&vote_count.gte=20&vote_average.gte=4&with_runtime.gte=60`
-    );
-    const data = await response.json();
-    return data.results;
-  } catch (error) {
-    console.log(error);
-  }
+export const fetchNewMovies = async (page = 1) => {
+  const response = await fetch(
+    `${baseUrl}discover/movie?api_key=${process.env.REACT_APP_API}&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=${page}&release_date.lte=2020-03-08&vote_count.gte=20&vote_average.gte=4&with_runtime.gte=60`
+  );
+  const data = await response.json();
+  return data;
 };
 
-export const searchMovie = async query => {
-  try {
-    const response = await fetch(
-      `${baseUrl}search/movie?api_key=${process.env.REACT_APP_API}&query=${query}`
-    );
-    const data = await response.json();
-    return data.results;
-  } catch (error) {
-    console.log(error);
-  }
+export const searchMovie = async (query = "", page = 1) => {
+  const response = await fetch(
+    `${baseUrl}search/movie?api_key=${process.env.REACT_APP_API}&query=${query}&page=${page}`
+  );
+  const data = await response.json();
+  return data;
 };
 
 export const getMovieDetails = async movieId => {
-  try {
-    const response = await fetch(
-      `${baseUrl}movie/${movieId}?api_key=${process.env.REACT_APP_API}&language=en-US`
-    );
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await fetch(
+    `${baseUrl}movie/${movieId}?api_key=${process.env.REACT_APP_API}&language=en-US`
+  );
+  const data = response.json();
+  return data;
 };
 
 export const getCast = async movieId => {
