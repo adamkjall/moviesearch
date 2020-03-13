31484aeffc882c5d98f955d92888f00737464605
Stenis76 <stenqvist.dick@gmail.com>
2020-03-13 12:09:32 +0100

    sidebar animate

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import "./sidebar.styles.css";
+import { Spring, config } from "react-spring/renderprops";
 
 class Sidebar extends React.Component {
   render() {
     return (
-      <div className="sidebarContainer">
-        {this.props.children}
+      <div>
+        <Spring
+          config={config.stiff}
+          from={{ opacity: 0, width: "20px" }}
+          to={{ opacity: 1, width: "250px" }}
+        >
+          {props => (
+            <div style={props} className="sidebarContainer">
+              {this.props.children}
+            </div>
+          )}
+        </Spring>
       </div>
     );
   }
